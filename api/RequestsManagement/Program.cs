76a201943403f30996de0ec0d89e8443434ff6f7
tsiviv <t0527698098@gmail.com>
2026-08-24 14:56:01 +0300

    remove claud

--- api/RequestsManagement/Program.cs
+++ api/RequestsManagement/Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddControllers()
 
 builder.Services.AddProblemDetails();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
